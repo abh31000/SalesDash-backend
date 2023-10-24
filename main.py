1d0a8b1d05d2db6f7516c08a71969413261ef5f6
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-10-24 20:22:23 +0100

    Added data endpoints Added pandas to dependencies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
@@ -6,3 +7,18 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+df = pd.read_csv("data.csv")
+
+@app.get("/data1")
+async def get_data1():
+    somme_annee = df.groupby('Année')['Ventes'].sum().reset_index()
+    return somme_annee.to_json(orient='records')
+
+@app.get("/data2")
+async def get_data2():
+    months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"]
+    df['Mois'] = pd.Categorical(df['Mois'], categories=months, ordered=True)
+    somme_mois = df.groupby('Mois')['Ventes'].sum().reset_index()
+    return somme_mois.to_json(orient='records')
